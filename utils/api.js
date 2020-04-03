6f7435a38ed2b098f5b57610c99f8e5b27ab9880
Christian Balentine <christianbalentine47@gmail.com>
2020-04-02 20:43:29 -0400

    fixed user avatar not showing as image.

--- utils/api.js
+++ utils/api.js
@@ -7,10 +7,10 @@ const api = {
     const queryUrl = `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
     axios.get(queryUrl)
     .then((res) => {
-      let userInfo = "User Name: " + res.data.login + "\n" + 
-        res.data.avatar_url + ".png" + "\n" + "User Email: " + res.data.email;
+      let userInfo = "User Name: " + res.data.login + "\n" + "![user avatar](" +
+        res.data.avatar_url + ".png)" + "\n" + "User Email: " + res.data.email;
 
-      fs.appendFile("user-README.md", "User Info: \n" + userInfo + "\n", (err) => {
+      fs.appendFile("user-README.md", "\n" + userInfo + "\n", (err) => {
         if (err) throw err;
       });
     })
